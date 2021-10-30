dcf8eee7edeaecafa131299921865dc302f71748
Dimitrije Djukic <dimitrijedjkc@gmail.com>
2021-10-30 17:07:30 +0200

    second commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Vue Calculator</title>
+</head>
+<body>
+</body> 
+
+<div id="app">
+    <h1>Calculator</h1>
+    <h2>Click 'change' for different mode</h2>
+  
+</div>
+<script src="./calc.js"></script>
+</body>
+</html>
